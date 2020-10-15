ca55755238062171651526a312c20296df1f73f1
jontjauregui <72755284+jontjauregui@users.noreply.github.com>
2020-10-14 17:33:37 -0700

    Add files via upload

---
+++ examen 1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace examen_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pant = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(522, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Eliminar boton";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(180, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(205, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Numero nuevo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pant
+            // 
+            this.pant.AutoSize = true;
+            this.pant.Location = new System.Drawing.Point(144, 170);
+            this.pant.Name = "pant";
+            this.pant.Size = new System.Drawing.Size(52, 13);
+            this.pant.TabIndex = 2;
+            this.pant.Text = "Numeros:";
+            this.pant.Click += new System.EventHandler(this.x_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 497);
+            this.Controls.Add(this.pant);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label pant;
+    }
+}
+
---
+++ examen 1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace examen_1
+{
+    public partial class Form1 : Form
+    {
+        int a ;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int b;
+            a = a + 1;
+            pant.Text = "";
+            if (a <= 25)
+            {
+                for (int i = 1; i <= a; i++)
+                {
+                    b = i;
+
+                    pant.Text = pant.Text + b + ", ";
+
+                }
+            }
+        }
+
+        private void x_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int b;
+            a = a - 1;
+            pant.Text = "";
+            if (a <= 25)
+            {
+                for (int i = 1; i <= a; i++)
+                {
+                    b = i;
+
+                    pant.Text = pant.Text + b + ", ";
+
+                }
+            }
+        }
+    }
+}
